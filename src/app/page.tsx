defc1c725bc800ac62a7690fd08260283af1d409
Matheus <48843124+hedacodes@users.noreply.github.com>
2024-02-19 19:15:14 -0300

    change favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="h-screen p-2">
+      <div className="flex h-full flex-col items-center justify-between p-8 rounded-3xl" style={{ backgroundColor: '#FF99C8' }}>
+
+        <div />
+
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-black	text-7xl text-center font-nunito">Quieeeeeeeetly.</h1>
+
+          <br /><br />
+
+          <button className="bg-black px-7 py-3 rounded-3xl font-bold	text-sm font-nunito">
+            get quietly ✌️
+          </button>
+        </div>
+
+        <nav className="text-xs">
+          <span className="px-2 py-3 text-gray-500">@{new Date().getFullYear()}</span>
+          <span className="px-2 py-3 text-gray-500">hello@quietly.space</span>
+          <button>
+            <span className="text-gray-600 px-2 py-3">Terms</span>
+          </button>
+          <button>
+            <span className="text-gray-600 px-2 py-3">Privacy</span>
+          </button>
+        </nav>
+
+      </div>
     </main>
   );
 }
