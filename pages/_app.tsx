aa03fd99b9b181ca871ec9b5a295af2439cbf49e
Matheus <48843124+hedacodes@users.noreply.github.com>
2024-06-07 14:40:45 -0300

    add profile page

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../style/globals.css'
+// import "../style/common.css";
+
+function MyApp({ Component, pageProps }: any) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
